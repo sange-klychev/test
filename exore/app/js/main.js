8f927a0f01092095d75d9fa70cf5910ddd3221a7
Sanjar <62173123+sange-klychev@users.noreply.github.com>
2021-04-16 00:50:53 +0300

    feat: test

---
+++ exore/app/js/main.js
@@ -0,0 +1,41 @@
+window.addEventListener('DOMContentLoaded', () => {
+  const page = document.querySelector('.page');
+  const burgerBtn = document.querySelector('.menu__burger');
+  const menuList = document.querySelector('.menu__list');
+  const menuLink = document.querySelectorAll('.menu__link');
+  const popupWrapper = document.querySelector('.popup-wrapper');
+  const popup = document.querySelector('.popup');
+  const btns = document.querySelectorAll('.btn');
+
+  burgerBtn.addEventListener('click', e => {
+    e.stopPropagation();
+    menuList.classList.toggle("open")
+  })
+
+  menuLink.forEach(link => {
+    link.addEventListener('click', e => {
+      e.stopPropagation();
+      menuLink.forEach(item => {
+        item.classList.remove('active');
+      })
+      link.classList.add("active");
+    })
+  })
+  btns.forEach(button => {
+    button.addEventListener('click', e => {
+      e.stopPropagation();
+      togglePopup();
+    })
+  })
+  document.addEventListener('click', e => {
+    if(!e.path.includes(popup)){
+      popupWrapper.classList.remove('popup--open');
+      page.classList.remove('popup--open');
+    }
+  })
+
+  function togglePopup() {
+    popupWrapper.classList.toggle('popup--open');
+    page.classList.toggle('popup--open');
+  }
+});
